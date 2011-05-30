f0f7636fd47e597ea2fb6785087daead68fb386e
adpauls@gmail.com <adpauls@gmail.com@fef7e017-a579-b2f4-8f1c-b670a06869aa>
2011-05-30 17:07:09 +0000

    some tests

--- trunk/test/edu/berkeley/nlp/lm/io/KneserNeyFromTextReaderTest.java
+++ trunk/test/edu/berkeley/nlp/lm/io/KneserNeyFromTextReaderTest.java
@@ -1,8 +1,13 @@
 package edu.berkeley.nlp.lm.io;
 
+import java.io.File;
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -13,17 +18,36 @@ import edu.berkeley.nlp.lm.values.KneseryNeyCountValueContainer.KneserNeyCounts;
 
 public class KneserNeyFromTextReaderTest
 {
+
 	@Test
 	public void myTest() {
-		String[] lines = new String[] { "This is a test sentence.", "This is another test sentence." };
 		final StringWordIndexer wordIndexer = new StringWordIndexer();
 		wordIndexer.setStartSymbol("<s>");
 		wordIndexer.setEndSymbol("</s>");
 		wordIndexer.setUnkSymbol("<unk>");
-		HashNgramMap<KneserNeyCounts> countNgrams = KneserNeyFromTextReader.countNgrams(wordIndexer, 3, Arrays.asList(lines));
+		File txtFile = getFile("tiny_test.txt");
+		File arpaFile = getFile("tiny_test.arpa");
+		HashNgramMap<KneserNeyCounts> countNgrams = KneserNeyFromTextReader.readFromFiles(Arrays.asList(txtFile), wordIndexer, 3);
 		final StringWriter stringWriter = new StringWriter();
 		KneserNeyFromTextReader.writeToPrintWriter(wordIndexer, countNgrams, new PrintWriter(stringWriter));
 		System.out.println(stringWriter.toString());
+		List<String> arpaLines = Arrays.asList(stringWriter.toString().split("\n"));
+		Collections.sort(arpaLines);
+	}
+
+	/**
+	 * @param testFileName
+	 * @return
+	 */
+	private File getFile(final String testFileName) {
+		File txtFile = null;
+		try {
+			txtFile = new File(KneserNeyFromTextReaderTest.class.getResource(testFileName).toURI());
+		} catch (URISyntaxException e) {
+			Assert.fail(e.toString());
+		}
+		Assert.assertNotNull("Could not read " + testFileName, txtFile);
+		return txtFile;
 	}
 
 }
