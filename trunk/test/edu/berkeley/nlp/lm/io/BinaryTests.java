a8d7ca8b11e9e62c3d80d4f7f9efb94b231bd01d
adpauls@gmail.com <adpauls@gmail.com@fef7e017-a579-b2f4-8f1c-b670a06869aa>
2011-05-31 22:18:19 +0000

    binary tests

---
+++ trunk/test/edu/berkeley/nlp/lm/io/BinaryTests.java
@@ -0,0 +1,70 @@
+package edu.berkeley.nlp.lm.io;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import edu.berkeley.nlp.lm.ConfigOptions;
+import edu.berkeley.nlp.lm.ContextEncodedProbBackoffLm;
+import edu.berkeley.nlp.lm.ProbBackoffLm;
+import edu.berkeley.nlp.lm.StringWordIndexer;
+import edu.berkeley.nlp.lm.util.Logger;
+
+public class BinaryTests
+{
+	@Test
+	public void testContextEncodedBinary() {
+		final ConfigOptions configOptions = new ConfigOptions();
+		configOptions.unknownWordLogProb = 0.0f;
+		final ContextEncodedProbBackoffLm<String> lm = LmReaders.readContextEncodedLmFromArpa(FileUtils.getFile(PerplexityTest.BIG_TEST_ARPA).getPath(),
+			new StringWordIndexer(), configOptions, Integer.MAX_VALUE);
+		File tmpFile = null;
+		try {
+			tmpFile = File.createTempFile("berkeleylmtest", "binary");
+		} catch (IOException e) {
+			Assert.fail(e.toString());
+
+		}
+		if (tmpFile != null) {
+			tmpFile.deleteOnExit();
+			IOUtils.writeObjFileHard(tmpFile, lm);
+			@SuppressWarnings("unchecked")
+			ContextEncodedProbBackoffLm<String> readLm = (ContextEncodedProbBackoffLm<String>) IOUtils.readObjFileHard(tmpFile);
+			PerplexityTest.testContextEncodedLogProb(readLm, FileUtils.getFile(PerplexityTest.TEST_PERPLEX_TXT), PerplexityTest.TEST_PERPLEX_GOLD_PROB);
+			tmpFile.delete();
+		} else {
+			Assert.fail();
+		}
+	}
+
+	@Test
+	public void testArrayEncodedBinary() {
+		final ConfigOptions configOptions = new ConfigOptions();
+		configOptions.unknownWordLogProb = 0.0f;
+		for (boolean compress : new boolean[] { true, false }) {
+			final ProbBackoffLm<String> lm = LmReaders.readArrayEncodedLmFromArpa(FileUtils.getFile(PerplexityTest.BIG_TEST_ARPA).getPath(), compress,
+				new StringWordIndexer(), configOptions, Integer.MAX_VALUE);
+			File tmpFile = null;
+			try {
+				tmpFile = File.createTempFile("berkeleylmtest", "binary");
+			} catch (IOException e) {
+				Assert.fail(e.toString());
+
+			}
+			if (tmpFile != null) {
+				tmpFile.deleteOnExit();
+				IOUtils.writeObjFileHard(tmpFile, lm);
+				@SuppressWarnings("unchecked")
+				ProbBackoffLm<String> readLm = (ProbBackoffLm<String>) IOUtils.readObjFileHard(tmpFile);
+				PerplexityTest.testArrayEncodedLogProb(readLm, FileUtils.getFile(PerplexityTest.TEST_PERPLEX_TXT), PerplexityTest.TEST_PERPLEX_GOLD_PROB);
+				tmpFile.delete();
+			} else {
+				Assert.fail();
+			}
+		}
+	}
+}
